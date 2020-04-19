5ea32b0476fd9e3edbde4ecd96086a9ea5364206
Andres Guachisaca <cguachis@pichincha.com>
2020-04-19 17:22:22 -0500

    add useTransition

--- src/useTransition/useTransition.js
+++ src/useTransition/useTransition.js
@@ -1,11 +1,13 @@
 import React, { useRef, useState } from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
-import Animated, { Transition, Transitioning } from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
+import { useTransition } from 'react-native-redash';
 
 import StyleGuide from '../components/StyleGuide';
 import Button from '../components/Button';
 import { FlexibleCard as Card, cards } from '../components/Card';
 
+const { multiply, interpolate, not } = Animated;
 const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
@@ -21,19 +23,36 @@ const styles = StyleSheet.create({
   },
 });
 
+const transformOrigin = -1 * (width / 2 - StyleGuide.spacing * 2);
+
 export default () => {
   const [toggled, setToggled] = useState(0);
+  const transition = useTransition(toggled, not(toggled), toggled);
   return (
     <View style={styles.container}>
       {cards.map((card, index) => {
-        const rotate = toggled ? Math.PI / 6 : 0;
+        const direction = interpolate(index, {
+          inputRange: [0, 1, 2],
+          outputRange: [-1, 0, 1],
+        });
+        const rotate = multiply(
+          direction,
+          interpolate(transition, {
+            inputRange: [0, 1],
+            outputRange: [0, Math.PI / 6],
+          }),
+        );
         return (
           <Animated.View
             key={card.id}
             style={[
               styles.overlay,
               {
-                transform: [{ rotate: `${rotate}rad` }],
+                transform: [
+                  { translateX: transformOrigin },
+                  { rotate },
+                  { translateX: -transformOrigin },
+                ],
               },
             ]}>
             <Card {...{ card }} />
